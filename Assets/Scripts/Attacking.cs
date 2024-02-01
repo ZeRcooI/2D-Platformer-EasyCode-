a6f4e367f651bec31f68c6274643b36ed489c0e5
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-01 16:44:41 +0300

    Добавил Тайлы на на ловушки

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Attacking : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
         }
