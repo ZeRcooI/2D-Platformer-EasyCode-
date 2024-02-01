32b76ba6b0b87b50c865a08e0428f291e5f3e25e
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-01 19:12:50 +0300

    Добавили анимации для ловушки и персонажа

--- Assets/Scripts/EnemyMovie.cs
+++ Assets/Scripts/EnemyMovie.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class EnemyMovie : MonoBehaviour
 {
-    public float speed;
+    SpriteRenderer spriteRenderer; //создаем компонент поворота
+
+    public float speed = 2f;
     public float timer = 0; // переменна€ таймера, котора€ используетс€ дл€ отсчета времени движени€ в определенном направлении.
-    public float moveDuration = 2; // переменна€, определ€юща€ длительность времени, в течение которой объект будет двигатьс€ в одном направлении, прежде чем изменить направление.
-    public Vector2 direction = Vector2.left; // переменна€, котора€ задает начальное направление движени€ объекта. ¬ данном случае, объект будет двигатьс€ влево при старте.
+    public float moveDuration = 5; // переменна€, определ€юща€ длительность времени, в течение которой объект будет двигатьс€ в одном направлении, прежде чем изменить направление.
+    public Vector2 direction = Vector2.right; // переменна€, котора€ задает начальное направление движени€ объекта. ¬ данном случае, объект будет двигатьс€ вправо при старте.
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();  //находим компонент поворота
+    }
 
     void Update()
     {
@@ -17,6 +24,12 @@ public class EnemyMovie : MonoBehaviour
         {
             direction = direction * -1; //≈сли условие выполн€етс€, то направление(direction) измен€етс€ на противоположное(умножение на - 1).
             timer = 0; //“аймер сбрасываетс€ до нул€, начина€ отсчет времени дл€ следующего движени€ в новом направлении.
+
+            //Ётот код использует компонент SpriteRenderer, чтобы изменить свойство flipX и перевернуть персонажа по горизонтали.
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
         }
     }
 }
