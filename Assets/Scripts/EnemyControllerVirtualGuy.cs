0aa091b40dfb71ff14b69fd74e3cd0c1e0cc2f61
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-01 21:44:13 +0300

    Добавили новый скрипт для второго врага, начали делать второго врага

--- Assets/Scripts/EnemyControllerVirtualGuy.cs
+++ Assets/Scripts/EnemyControllerVirtualGuy.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemyMovie : MonoBehaviour
+public class EnemyControllerVirtualGuy : MonoBehaviour
 {
     SpriteRenderer spriteRenderer; //создаем компонент поворота
 
