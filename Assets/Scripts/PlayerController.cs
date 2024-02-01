457de51c3fde3a6abc10cdcce8822448b174d4a3
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-01 19:49:51 +0300

    Добавили анимацию бега и объединили условие

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,60 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
 
     public float speed = 10f;
-    public float jumpforce = 8f; 
+    public float jumpforce = 8f;
 
+    public Animator animator;
 
-    void Start()
+void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        // Объединенное условие для проверки нажатия клавиши A или D
+        bool isMoving = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+
+        // Установка анимации в зависимости от того, идет ли игрок влево или вправо
+        animator.SetBool("isRuning", isMoving);
+
+        // Перемещение игрока влево или вправо
+        if (isMoving)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(speed * Time.deltaTime * Vector2.left);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(speed * Time.deltaTime * Vector2.right);
+            }
+        }
+
         //transform.Translate используется для перемещения объекта в пространстве по указанному вектору.
         //Vector2.left - это вектор, указывающий влево(отрицательное направление по оси X).
 
         //Time.deltaTime - это значение, представляющее время, прошедшее с момента последнего кадра.
         //Использование Time.deltaTime обеспечивает плавное движение объекта независимо от фреймрейта игры.
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(speed * Time.deltaTime * Vector2.left);
-        }
+        //Условие для перехода в бег
+        //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        //{
+        //    animator.SetBool("isRuning", true);
+        //}
+        //else
+        //{
+        //    animator.SetBool("isRuning", false);
+        //}
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(speed * Time.deltaTime * Vector2.right);
-        }
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(speed * Time.deltaTime * Vector2.left);
+        //}
+        //else if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(speed * Time.deltaTime * Vector2.right);
+        //}
 
         //GetKeyDown - это метод, который можно использовать для определения, была ли нажата клавиша на клавиатуре в данный момент времени.
         //можем настроить гравитацию для нормального прыжка
