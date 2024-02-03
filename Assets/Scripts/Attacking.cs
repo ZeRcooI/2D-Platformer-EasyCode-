65b6d4a649cc85462016287a27dfedb1371042ad
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-03 21:55:23 +0300

    Обновли скрипт Attacking(перезапуск сцены)

--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Attacking : MonoBehaviour
 {
@@ -7,6 +8,38 @@ public class Attacking : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+
+            ReloadScene();
         }
     }
+
+    private void ReloadScene()
+    {
+        // Получаем индекс текущей сцены
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Перезагружаем текущую сцену
+        SceneManager.LoadScene(currentSceneIndex);
+    }
 }
+
+//private void OnTriggerEnter2D(Collider2D collision)
+//{
+//    if (collision.CompareTag("Player"))
+//    {
+//        // Уничтожаем игрока
+//        Destroy(collision.gameObject);
+
+//        // Перезагружаем текущую сцену
+//        ReloadScene();
+//    }
+//}
+
+//private void ReloadScene()
+//{
+//    // Получаем индекс текущей сцены
+//    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+//    // Перезагружаем текущую сцену
+//    SceneManager.LoadScene(currentSceneIndex);
+//}
