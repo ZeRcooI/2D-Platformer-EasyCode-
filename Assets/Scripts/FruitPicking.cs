f86989f87c8a291c17cb6aa58cec14b20ab667af
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-03 21:04:12 +0300

    Обновлена логика скрипта подбора UI

--- Assets/Scripts/FruitPicking.cs
+++ Assets/Scripts/FruitPicking.cs
@@ -4,23 +4,18 @@ using UnityEngine.UI; //подключаем канвас
 
 public class FruitPicking: MonoBehaviour
 {
-    [SerializeField] private int _score;
+    private int _score = 0;
     [SerializeField] private TextMeshProUGUI _textMeshPro;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-        }
-
         if (collision.CompareTag("fruits"))
         {
             _score += 1;
 
-            Destroy(collision.gameObject);
-
             _textMeshPro.text = _score.ToString(); // связываем и конвертим очки в стринг
+
+            Destroy(collision.gameObject);
         }
     }
 }
