fe4c3b3940ca860b8611c2cb07a0db18ee570428
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-03 20:41:03 +0300

    Добавили канвас , подключаем скрипты к фруктам

--- Assets/Scripts/FruitPicking.cs
+++ Assets/Scripts/FruitPicking.cs
@@ -1,12 +1,26 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI; //подключаем канвас
 
 public class FruitPicking: MonoBehaviour
 {
+    [SerializeField] private int _score;
+    [SerializeField] private TextMeshProUGUI _textMeshPro;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
+        }
+
+        if (collision.CompareTag("fruits"))
+        {
+            _score += 1;
+
+            Destroy(collision.gameObject);
+
+            _textMeshPro.text = _score.ToString(); // связываем и конвертим очки в стринг
         }
     }
 }
