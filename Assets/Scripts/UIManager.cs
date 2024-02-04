464b725fd7acb3e6299bea71f294bd8dd13ff33d
ZeRcooI <94679102+ZeRcooI@users.noreply.github.com>
2024-02-04 14:40:48 +0300

    Добавили заключительную сцену и кнопки на ней с текстом

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,7 +5,11 @@ public class UIManager : MonoBehaviour
 {
     public void RestartLevel()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
+    public void ExitGame() //Этот метод завершает выполнение приложения. Обращаем внимание, что в режиме редактора Unity или в веб-плеере, эта команда может не работать. 
+    {
+        Application.Quit(); //метод, который используется для завершения выполнения приложения. Когда этот метод вызывается, приложение завершается, и игра закрывается.
+    }
 }
